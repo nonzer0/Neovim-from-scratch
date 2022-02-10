0408764af04467def72c9dc5e28ea5f97d4583f8
Neil West <nwest@turnitin.com>
2022-02-10 13:20:41 -0500

    feat: neoformat, keymappings

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -32,8 +32,7 @@ keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", ",,", "<C-^>", opts)
 
 -- Move text up and down
 keymap("n", "<C-j>", "<Esc>:m .+1<CR>==gi", opts)
@@ -66,4 +65,3 @@ keymap("x", "<C-k>", ":move '<-2<CR>gv-gv", opts)
 -- keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 -- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 -- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
-
---
+++ lua/user/neoformat.lua
@@ -0,0 +1,4 @@
+vim.api.nvim_exec([[ augroup fmt
+  autocmd!
+  autocmd BufWritePre * undojoin | Neoformat
+augroup END]], false)
